6968e0252d85e49fbfbcf70c72e8b21ce683cf76
edu <eduaro.c@novaworkscom.ar>
2024-10-25 23:37:31 -0300

    loop interval to root scope

--- server.js
+++ server.js
@@ -24,28 +24,38 @@ app.use(express.static('public'));
 let currentStep = 0; // Paso actual
 let connectedClients = 0; // Contador de clientes conectados
 
+// Iniciar un bucle de reproducción en el servidor
+const loop = setInterval(() => {
+    currentStep = (currentStep + 1) % 16; // Avanzar al siguiente paso
+    // io.emit('stepChanged', currentStep); // Emitir el paso actual a todos los clientes
+
+    if (currentStep === 0) {
+        io.emit('startSequence'); // Emitir señal de inicio de secuencia
+    }
+}, 100); // Cambia el tiempo según la velocidad deseada
+
 // Configuración de Socket.IO
 io.on('connection', (socket) => {
     connectedClients++; // Incrementar el contador al conectar
-    console.log('Nuevo cliente conectado. Total de clientes conectados:', connectedClients);
+    console.log('Nuevo cliente. Total:', connectedClients);
 
     // Emitir el paso actual al nuevo cliente
     // socket.emit('currentStep', currentStep);
 
     // Iniciar un bucle de reproducción
-    const loop = setInterval(() => {
-        currentStep = (currentStep + 1) % 16; // Avanzar al siguiente paso
-        // io.emit('stepChanged', currentStep); // Emitir el paso actual a todos los clientes
-        if (currentStep == 0) {
-            // console.log(currentStep)
-            io.emit('startSequence'); // Emitir el paso actual a todos los clientes
-        }
-    }, 100); // Cambia el tiempo según la velocidad deseada
+    // const loop = setInterval(() => {
+    //     currentStep = (currentStep + 1) % 16; // Avanzar al siguiente paso
+    //     // io.emit('stepChanged', currentStep); // Emitir el paso actual a todos los clientes
+    //     if (currentStep == 0) {
+    //         // console.log(currentStep)
+    //         io.emit('startSequence'); // Emitir el paso actual a todos los clientes
+    //     }
+    // }, 100); // Cambia el tiempo según la velocidad deseada
 
     socket.on('disconnect', () => {
         connectedClients--; // Decrementar el contador al desconectar
         console.log('Cliente desconectado. Total de clientes conectados:', connectedClients);
-        clearInterval(loop); // Detener el bucle si el cliente se desconecta
+        // clearInterval(loop); // Detener el bucle si el cliente se desconecta
     });
 });
 
